1ef3543803ba82294013c098c0945a12bf0eb63b
Game King <thomasa@pop-os.localdomain>
2023-09-19 11:26:19 -0700

    Player health and collision adjustments

--- Source/Enemies/enemy.gd
+++ Source/Enemies/enemy.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 @export
 var enemy_speed = 10.0
+@export
+var enemy_collision_layer = 0x0002
+
+signal _on_non_enemy_collision(Node2D)
 
 var target_node : Node2D
 var initialized = false
@@ -16,8 +20,15 @@ func _physics_process(delta):
 	
 	var move_dir = (target_node.global_position - global_position).normalized()
 	velocity = move_dir * enemy_speed
-	move_and_slide()
+	var collided = move_and_slide()
+	if collided:
+		_handle_collision()
 
+func _handle_collision():
+	for i in get_slide_collision_count():
+		var collision_obj = get_slide_collision(i).get_collider()
+		if not collision_obj.collision_layer & int(pow(2, enemy_collision_layer-1)):
+			_on_non_enemy_collision.emit(collision_obj)
 
 func _on_health_counter_health_depleted():
 	queue_free()
