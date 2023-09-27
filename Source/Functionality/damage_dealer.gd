992f7d1e7cb667419b679f51e08d6e4832b699b3
Game King <thomasa@pop-os.localdomain>
2023-09-26 21:37:22 -0700

    FIREBALL Behavior and DamageDealer refactoring

--- Source/Functionality/damage_dealer.gd
+++ Source/Functionality/damage_dealer.gd
@@ -5,11 +5,30 @@ var damage = 10.0
 
 var health_node_path = ^"HealthCounter"
 
-func damage_node(node_to_damage : Node2D):
+# damage < 0 = instant kill
+func damage_node_by_amount(node_to_damage : Node2D, damage : float):
+	if !node_to_damage.has_method("get_node"):
+		push_warning("Tried to damage something that isn't a Node")
+		return
+
 	var health = node_to_damage.get_node(health_node_path)
 	
 	if health:
+		if damage < 0:
+			damage = health.max_health * 10
+			
 		health.decrease_health(damage)
+	else:
+		push_warning("Tried to damage %s, but it has no HealthCounter" % node_to_damage.to_string())
+		
+func damage_node(node_to_damage : Node2D):
+	damage_node_by_amount(node_to_damage, damage)
+
+func instant_kill_node(node_to_kill):
+	damage_node_by_amount(node_to_kill, -1)
 
 func _on_damage_triggered(node_to_damage : Node2D):
 	damage_node(node_to_damage)
+	
+func _on_instant_kill_triggered(node_to_kill : Node2D):
+	instant_kill_node(node_to_kill)
---
+++ Source/Spells/fireball_spell.gd
@@ -0,0 +1,57 @@
+extends Node2D
+
+@export
+var explosion_position_offset : Vector2
+@export_flags_2d_physics
+var fireball_collision_mask
+
+@onready
+var spell_mover = $LinearSpellMover
+@onready
+var damage_dealer = $DamageDealer
+
+var screen_center : Vector2
+var screen_size : Vector2
+var explosion_position : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var viewport_rect = get_viewport().get_visible_rect()
+	screen_size = viewport_rect.size
+	screen_center = viewport_rect.position + (screen_size / 2)
+	explosion_position = screen_center + explosion_position_offset
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var dir_to_center = get_dir_to_explosion_position()
+	
+	# checking vector dots is slightly more expensive than checking position,
+	# but it will work even if the spell overshoots the exact position because
+	# of subpixel movement or whatever else.
+	if global_transform.x.dot(dir_to_center) <= 0:
+		kill_all_enemies()
+
+func get_dir_to_explosion_position() -> Vector2:
+	return (explosion_position - global_position).normalized()
+		
+func launch():
+	# rotate towards screen center
+	var dir_to_center = get_dir_to_explosion_position()
+	global_transform.x = dir_to_center
+	global_transform.y = dir_to_center.orthogonal()
+	
+	spell_mover.launch()
+	
+func kill_all_enemies():
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = RectangleShape2D.new()
+	query.shape.set_size(screen_size)
+	query.collision_mask = fireball_collision_mask
+	query.transform = Transform2D(0.0, screen_center)
+	
+	for result in space_state.intersect_shape(query):
+		if result.collider:
+			damage_dealer.instant_kill_node(result.collider)
+
+	queue_free()
