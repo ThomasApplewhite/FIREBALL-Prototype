5bffebcd3eed734ee0211e2f42d12c4b4cd1aa0e
Game King <thomasa@pop-os.localdomain>
2023-11-28 21:13:34 -0800

    Quit Buttons

--- Source/GameScenes/main_menu.gd
+++ Source/GameScenes/main_menu.gd
@@ -4,5 +4,12 @@ extends Control
 var packed_gameplay_scene : PackedScene
 
 
-func _on_button_pressed():
+func _on_start_button_pressed():
 	get_tree().change_scene_to_packed(packed_gameplay_scene)
+
+
+func _on_quit_button_pressed():
+	# Go Back is for Android, Close is for everything else
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().root.propagate_notification(NOTIFICATION_WM_GO_BACK_REQUEST)
+	get_tree().quit()
--- Source/UI/pause_menu_control.gd
+++ Source/UI/pause_menu_control.gd
@@ -21,3 +21,10 @@ func toggle_pause():
 
 func _on_pause_button_pressed():
 	toggle_pause()
+
+
+func _on_quit_button_pressed():
+	# Go Back is for Android, Close is for everything else
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().root.propagate_notification(NOTIFICATION_WM_GO_BACK_REQUEST)
+	get_tree().quit()
